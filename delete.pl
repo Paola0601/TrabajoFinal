cb82136961d842b31afb542ea702ed114d721237
rodrigo <rfloresnu@unsa.edu.pe>
2024-12-04 11:55:20 -0500

    Mensaje en caso q no exista

--- delete.pl
+++ delete.pl
@@ -7,18 +7,19 @@ print "Content-type: text/html; charset=UTF-8\n\n";
 
 # Obtener el nombre de la página desde la URL
 my $query = param('fn');
-$query =~ s/[^a-zA-Z0-9_-]//g;  # Sanitizar el nombre del archivo
+$query =~ s/[^a-zA-Z0-9_-]//g;
 my $file = "pages/$query.md";
 
 if (-e $file) {
-    # Eliminar el archivo
     unlink($file) or die "No se pudo eliminar el archivo: $!";
 
-    
     print "<h1>Pagina eliminada</h1>";
     print "<p>La pagina '$query' ha sido eliminada exitosamente.</p>";
     print "<p><a href='list.pl'>Volver al Listado</a></p>";
-
+} else {
+    print "<p>La pagina no existe.</p>";
+    print "<p><a href='list.pl'>Volver al Listado</a></p>";
+}
 
 
 
