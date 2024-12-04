80262e06b058fe248213ef5de0831260cb56e253
rodrigo <rfloresnu@unsa.edu.pe>
2024-12-04 11:49:58 -0500

    Se verifica y elimina el archivo

--- delete.pl
+++ delete.pl
@@ -10,3 +10,14 @@ my $query = param('fn');
 $query =~ s/[^a-zA-Z0-9_-]//g;  # Sanitizar el nombre del archivo
 my $file = "pages/$query.md";
 
+if (-e $file) {
+    # Eliminar el archivo
+    unlink($file) or die "No se pudo eliminar el archivo: $!";
+
+
+
+
+
+
+
+    
