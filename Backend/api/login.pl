0743a0c51b421c8af9482af1870d79387af31900
Nato <rponcel@unsa.edu.pe>
2024-12-11 12:00:22 -0500

    Se avanza la conexion con la base de datos, el registro y el login

--- Backend/api/login.pl
+++ Backend/api/login.pl
@@ -2,4 +2,28 @@
 use strict;
 use warnings;
 use CGI;
-use DBI;
+require "db_config.pl";
+
+my $cgi = CGI->new;
+print $cgi->header('application/json');
+
+my $email = $cgi->param('email');
+my $password = $cgi->param('password');
+
+if (!$email || !$password) {
+    print '{"error": "Email y contraseña son requeridos"}';
+    exit;
+}
+
+my @conexion = ($email,$password);
+
+#Verificamos que sea un usuario de nuestra tienda
+my $dbh = conectar_db();
+my $sth = $dbh->prepare("SELECT id_usuario FROM usuarios WHERE correo = ? AND contraseña = ?");
+$sth->execute($email, $password);
+if (my $row = $sth->fetchrow_hashref) {
+    print '{"success": "Inicio de sesión exitoso"}';
+} else {
+    print '{"error": "Credenciales incorrectas"}';
+}
+$dbh->disconnect;
--- Backend/db_config.pl
+++ Backend/db_config.pl
@@ -1,5 +1,21 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
-use CGI;
 use DBI;
+
+# Configuración de la conexión
+sub conectar_db {
+    my @datos = @_;
+    my $dsn = "DBI:mysql:database=farmacia;host=mariadb";
+    my $usuario = $datos[0];
+    my $contrasena = $datos[1];
+
+    my $dbh = DBI->connect($dsn, $usuario, $contrasena, {
+        RaiseError => 1,
+        AutoCommit => 1,
+    }) or die "No se pudo conectar a la base de datos: $DBI::errstr";
+    
+    return $dbh;
+}
+
+1;
