8b8de3026653711373e4967113737fa48da524e0
rodrigo <rfloresnu@unsa.edu.pe>
2024-12-12 07:13:41 -0500

    Cambiar el tamaño de la barra de navegacion segun el scroll

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
     const navBar = document.querySelector(".navbar");
     window.addEventListener("scroll", () => {
+        if (window.scrollY > 50) {
+            navBar.style.padding = "10px 50px";
+        } else {
+            navBar.style.padding = "20px 50px";
+        }
+    });
