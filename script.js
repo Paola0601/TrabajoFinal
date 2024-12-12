897758004803f3f1bbf10c2c4b135c63a109aa92
rodrigo <rfloresnu@unsa.edu.pe>
2024-12-12 07:26:10 -0500

    Restaurar el tamñano de la imagen

--- script.js
+++ script.js
@@ -13,3 +13,7 @@ document.addEventListener("DOMContentLoaded", () => {
         img.addEventListener("mouseenter", () => {
             img.style.transform = "scale(1.1)";
         });
+        img.addEventListener("mouseleave", () => {
+            img.style.transform = "scale(1)";
+        });
+    });
