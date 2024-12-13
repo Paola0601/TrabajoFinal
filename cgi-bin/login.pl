fa638ee83351826ecaaedd677696a9358e8b770f
Paola0601 <pmaytaqu@unsa.edu.pe>
2024-12-13 16:58:57 -0500

    Cambios

--- cgi-bin/login.pl
+++ cgi-bin/login.pl
@@ -7,67 +7,23 @@ use DBI;
 my $cgi = CGI->new;
 print $cgi->header('text/html; charset=UTF-8');
 
+# Recibimos los parámetros del html
 my $correo = $cgi->param('email');
 my $password = $cgi->param('password');
 
-sub validar_correo {
-    my $correo = shift;
-    return $correo =~ /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-}
+# Hacemos la conexión con la base de datos
+my $database = "register";
+my $hostname = "mariadb";
+my $port = 3306;
+my $dsn = "DBI:mysql:database=$database;host=$hostname;port=$port";
+my $usuario = "root";
+my $contrasena = "contraseña";
 
-sub validar_contrasena {
-    my $contrasena = shift;
-    return length($contrasena) >= 8 && $contrasena =~ /[A-Za-z]/ && $contrasena =~ /\d/;
-}
+my $dbh = DBI->connect($dsn, $usuario, $contrasena, {
+    RaiseError => 1,
+    AutoCommit => 1,
+}) or die "No se pudo conectar a la base de datos: $DBI::errstr";
 
-# Validamos los campos del formulario
-if (!$correo || !$password) {
-    print "<!DOCTYPE html>";
-    print "<html><head><title>Errores de Registro</title>";
-    print "<link rel='stylesheet' href='../Fronted/estilos.css'></head><body>";
-    print "<p>Correo y contraseña deben estar llenos</p><ul>";
-    print "<a href='../Fronted/login.html'>Volver al formulario</a>";
-    print "</div></body></html>";
-    exit;
-}
-
-# Añadimos la validación que sea una contraseña correcta y que el correo sea válido
-if (validar_contrasena($contrasena) && validar_correo($correo)) {
-    # Conectamos con la base de datos
-    my $dsn = "DBI:mysql:database=register;host=mariadb";
-    my $usuario = "root";
-    my $contrasena = "contraseña";
-
-    my $dbh = DBI->connect($dsn, $usuario, $contrasena, {
-        RaiseError => 1,
-        AutoCommit => 1,
-    }) or die "No se pudo conectar a la base de datos: $DBI::errstr";
-    
-    # Realizamos la consulta
-    my $sth = $dbh->prepare("SELECT id_usuario FROM usuarios WHERE correo = ? AND contraseña = ?");
-    $sth->execute($correo, $password);
-
-    if (my $row = $sth->fetchrow_hashref) {
-        print "<!DOCTYPE html>";
-        print "<html><head><title>Errores de Registro</title>";
-        print "<link rel='stylesheet' href='../Fronted/estilos.css'></head><body>";
-        print "<p>Inicio de sesion exitoso</p><ul>";
-        print "<a href='../Fronted/login.html'>Volver al formulario</a>";
-        print "</div></body></html>";
-    } else {
-        print "<!DOCTYPE html>";
-        print "<html><head><title>Errores de Registro</title>";
-        print "<link rel='stylesheet' href='../Fronted/estilos.css'></head><body>";
-        print "<p>Credenciales incorrectas</p><ul>";
-        print "<a href='../Fronted/login.html'>Volver al formulario</a>";
-        print "</div></body></html>";
-    }
-    $dbh->disconnect;
-} else {
-    print "<!DOCTYPE html>";
-    print "<html><head><title>Errores de Registro</title>";
-    print "<link rel='stylesheet' href='../Fronted/estilos.css'></head><body>";
-    print "<p>Correo y contraseña no válidos</p><ul>";
-    print "<a href='../Fronted/login.html'>Volver al formulario</a>";
-    print "</div></body></html>";
-}
+my $sth = $dbh->prepare("SELECT id_usuario FROM usuarios WHERE correo = ? AND contraseña = ?");
+$sth->execute($correo,$password);
+$dbh->disconnect;
