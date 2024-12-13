ba14d4e3ced376927789c12067649179f6139500
rodrigo <rfloresnu@unsa.edu.pe>
2024-12-13 07:55:23 -0500

    Crear la función cargar productos

--- javaScript/indexJavaS.js
+++ javaScript/indexJavaS.js
@@ -1,1 +1,12 @@
 var productos =[];
+
+function cargarProductos() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', '/cgi-bin/recuperarproductos.pl', true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            productos = JSON.parse(xhr.responseText);
+            mostrarProductos(productos);
+        }
+    };
+    xhr.send();
