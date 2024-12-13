1a243bbcd23da15d8acc960a923f53c69d423531
rodrigo <rfloresnu@unsa.edu.pe>
2024-12-13 02:45:45 -0500

    Funcion para mostrar error de  forma dinamica

--- javaScript/registro.js
+++ javaScript/registro.js
@@ -7,4 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
         contrasenaInvalida: 'La contraseña debe tener al menos 8 caracteres, incluir letras y números.', // Commit: Agregar mensaje de error para contraseñas débiles
         credencialesIncorrectas: 'Credenciales incorrectas. Intenta nuevamente.' // Commit: Agregar mensaje de error para credenciales incorrectas
     };
-    
+    const mostrarError = (mensaje) => {
+        errorLoginLabel.style.color = 'red'; // Commit: Configurar el color del texto del mensaje de error a rojo
+        errorLoginLabel.textContent = mensaje; // Commit: Actualizar el contenido del mensaje de error con el mensaje proporcionado
+    };
