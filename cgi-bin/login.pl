d86ec886a26dc4cd0942771812cb526c72b8246b
Nato <rponcel@unsa.edu.pe>
2024-12-19 10:06:43 -0500

    Arreglo en el login.pl

--- cgi-bin/login.pl
+++ cgi-bin/login.pl
@@ -5,63 +5,36 @@ use CGI;
 use DBI;
 
 my $cgi = CGI->new;
-print $cgi->header('text/html; charset=UTF-8');
-
-# Recibimos los parámetros del html
-my $correo = $cgi->param('email');
+my $email = $cgi->param('email');
 my $password = $cgi->param('password');
 
-# Hacemos la conexión con la base de datos
-my $database = "data_base"; # Nombre de la base de datos
-my $hostname = "mariadb"; # Nombre del contenedor que la contiene
-my $port = 3306; # Puerto por defecto de MariaDB
-my $dsn = "DBI:mysql:database=$database;host=$hostname;port=$port";
-my $usuario = "root";
+# Conexión con la base de datos
+my $database = "data_base";
+my $hostname = "mariadb";
+my $port     = 3306;
+my $dsn      = "DBI:mysql:database=$database;host=$hostname;port=$port";
+my $usuario  = "root";
 my $contrasena = "paredes48621234";
 
 my $dbh = DBI->connect($dsn, $usuario, $contrasena, {
     RaiseError => 1,
     AutoCommit => 1,
+    mysql_enable_utf8 => 1,
 }) or die "No se pudo conectar a la base de datos: $DBI::errstr";
 
-# Realizamos la consulta a la base de datos
-my $sth = $dbh->prepare("SELECT FROM usuarios WHERE correo = ? AND contraseña = ?");
-$sth->execute($correo,$password);
-$sth->finish();
+# Verificación de credenciales
+my $sth = $dbh->prepare("SELECT * FROM usuarios WHERE correo = ? AND contraseña = ?");
+$sth->execute($email, $password);
+my @row = $sth->fetchrow_array;
+$sth->finish;
 
-if (my $row = $sth->fetchrow_hashref) {
-    # Usuario encontrado
-    print <<HTML;
-        <!DOCTYPE html>
-        <html lang="es">
-        <head>
-            <script>
-                localStorage.setItem('nombre_usuario', '$row->{nombre}');
-                window.location.href = '../index.html';
-            </script>
-        </head>
-        <body>
-            <h3>Inicio sesión correctamente puede volver al inicio</h3>
-            <a href="../index.html">Volver</a>
-        </body>
-        </html>
-HTML
+if (@row) {
+    # Si el login fue exitoso se redireccionará a la página de bienvenida
+    print $cgi->redirect("http://localhost:8118/html/bienvenida.html");
 } else {
-    # Error en el correo o la contraseña
-    print <<HTML;
-    <!DOCTYPE html>
-    <html lang="es">
-    <html>
-    <body>
-        <h3>Hubo un error, revise su correo y/o contraseña</h3>
-        <a href="../index.html">Volver</a>
-        <script>
-            window.location.href = '../login.html?error=1';
-        </script>
-    </body>
-    </html>
-HTML
+    # Si el login fue fallido se mostrará un error
+    print $cgi->header(-type => 'text/html', -status => '401 Unauthorized');
+    print "Credenciales incorrectas.";
 }
 
-# Cerramos la conexión con la base de datos
-$dbh->disconnect();
+$dbh->disconnect;
